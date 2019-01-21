2b29b7f16cea3507d8c9f1fa6fc0e61169bb90b2
JWKennington <jameswkennington@gmail.com>
2019-01-21 17:20:40 -0600

    .

--- apsjournals/__init__.py
+++ apsjournals/__init__.py
@@ -2,7 +2,7 @@ __MAJOR__ = 0
 __MINOR__ = 1
 __MICRO__ = 0
 __VERSION__ = (__MAJOR__, __MINOR__, __MICRO__)
-__version__ = ','.join(str(n) for n in __VERSION__)
+__version__ = '.'.join(str(n) for n in __VERSION__)
 
 
 from apsjournals.journals import PRL, PRM, PRA, PRB, PRC, PRD, PRE, PRX, PRAB, PRApplied, PRFluids, PRMaterials, PRPER
